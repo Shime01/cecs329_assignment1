bc9970f1a7e0ffe85eede59f0728193b1b82e2c1
Khirby Calma <calmagkhirby@gmail.com>
2024-03-04 13:17:09 -0800

    Implement dfa

--- Lab_Assignment.py
+++ Lab_Assignment.py
@@ -4,25 +4,91 @@ regex_L = r".*khirby.calma.*"
 
 import graphviz
 
-# Define DFA states
-states = ['q0', 'q1', 'q2', 'q3', 'q4']
+# Define alphabet (sigma) 
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
-# Define alphabet (summation) 
-# FIXME: the alphabet is not complete
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f']
+# Define transitions
+def createTransition(sigmaTransition, customTransitions = {}): 
+	transitions = {}
+	for letter in alphabet:
+		if letter in customTransitions:
+			transitions[letter] = customTransitions[letter]
+		else:
+			transitions[letter] = sigmaTransition
+	return transitions
 
-
-# Define transition
-# FIXME: the transition is not complete
-transition = {
-	'q0': {'a': 'q0', 'b': 'q0'},
-	'q1': {'h': 'q2'},
-	'q2': {'i': 'q3'},
-	'q3': {'r': 'q4'},
-	'q4': {'b': 'q4', 'a': 'q4'}
+qStatesAndTransitions = {
+	'q0': createTransition('q0', {'k': 'q1'}),
+	'q1': createTransition('q0', {'h': 'q2', 'k': 'q1'}),
+	'q2': createTransition('q0', {'i': 'q3'}),
+	'q3': createTransition('q0', {'r': 'q4'}),
+	'q4': createTransition('q0', {'b': 'q5'}),
+	'q5': createTransition('q0', {'y': 'q6'}),
+	'q6': createTransition('q6', {'c': 'q7'}),
+	'q7': createTransition('q6', {'a': 'q8', 'c': 'q7'}),
+	'q8': createTransition('q6', {'l': 'q9'}),
+	'q9': createTransition('q6', {'m': 'q10'}),
+	'q10': createTransition('q6', {'a': 'q11'}),
+	'q11': createTransition('q11'),
 }
 
-# Define initial and accepting states
-
-
 # Create and visualize the DFA
+def DFA(transitions, string):
+	startState = 'q0'
+	currentState = startState
+	acceptingState = 'q11'
+	for letter in string:
+		currentState = transitions[currentState][letter]
+	if (currentState == acceptingState):
+		return 'Accept'
+	else:
+		return 'Reject'
+
+# no randomsubstring 
+testString = 'khirbycalma'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Accept'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+# random substring front
+testString = 'dsfsuyfbqbidskkhirbycalma'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Accept'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+
+# random substring middle
+testString = 'khirbydfigufdguibdfvccalma'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Accept'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+# random substring end
+testString = 'khirbycalmaaciuerafbdskjfb'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Accept'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+# random substring everywhere
+testString = 'sdifhsuifsakkhirbysdofsofsoojccalmaaciuerafbdskjfb'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Accept'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+# empty string
+testString = ''
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Reject'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+# misspelled first name
+testString = 'kirbycalma'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Reject'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
+
+# misspelled last name
+testString = 'khirbycalm'
+dfaOutput = DFA(qStatesAndTransitions, testString)
+expectedOutput = 'Reject'
+print(f'String: {testString}\nDFA: {dfaOutput}\nExpected: {expectedOutput}\n')
